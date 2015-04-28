dae0b0f9a508b415f345720bdf5ae59f797b8301
Scott Lackey <slackey@salesforce.com>
2015-04-28 15:36:14 -0700

    adding module dependencies to spec dir for testing

---
+++ Puppetfile
@@ -0,0 +1,5 @@
+forge 'https://forgeapi.puppetlabs.com'
+
+mod 'puppetlabs/stdlib', '>=2.3.3'
+mod 'maestrodev/maven'
+mod 'maestrodev/wget'
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 describe 'exhibitor' do
   let(:title) { 'exhibitor' }
+  it { should contain_class('exhibitor') }
+  it { should compile.with_all_deps }
 
 end
