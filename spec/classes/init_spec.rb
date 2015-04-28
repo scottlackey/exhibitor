38ac7e9596adaf3ab777e76aa474c6620a6aa4da
Scott Lackey <slackey@salesforce.com>
2015-04-28 12:36:57 -0700

    modified testing

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,5 @@
 require 'spec_helper'
 describe 'exhibitor' do
+  let(:title) { 'exhibitor' }
 
-  context 'with defaults for all parameters' do
-    it { should contain_class('exhibitor') }
-  end
 end
